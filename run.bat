933121ba53077342fdc5a9f7605e58a14c0dba0c
Brenden Shutt <brend@rig.alexandria.server>
2024-06-08 14:32:12 -0400

    fix run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
-start "Map Tile Server" /d . python map_server.py localhost 1234 ./map_tiles/ESRI
+start "Map Tile Server" /d . python src/map_server.py localhost 1234 ./map_tiles/ESRI
 start "EW Targeting App" /d . python src/gui.py
