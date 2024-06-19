3420fa946010decf07e4f4201356c4a3d6dd0b9c
Brenden Shutt <brend@rig.alexandria.server>
2024-06-19 19:26:10 -0400

    added print statement to update.bat

--- update.bat
+++ update.bat
@@ -9,5 +9,9 @@ git fetch
 rem Pull the latest changes from the remote repository into the current branch
 git pull
 
+rem Print that the code base was updated
+echo.
+echo Code Base was updated!
+
 rem Sleep for 5 seconds
 ping 127.0.0.1 -n 6 > nul
